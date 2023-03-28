1a4ac96239ae8c32f9b2502a8f974d5aebe0f3ba
v khanhn <vankhanhth15@gmail.com>
2023-03-28 20:04:42 +0700

    demo constructor to data

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,54 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Demo"),
         ),
-        body: Container(),
+        body: OngBa(),
       )
     );
   }
 }
+
+class OngBa extends StatelessWidget {
+
+  int count = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ChaMe(count),
+    );
+  }
+}
+
+class ChaMe extends StatelessWidget {
+
+  //count để hứng data từ ông bà
+  late int count;
+  //Constructor ChaMe
+  ChaMe(this.count);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ConGai(count),
+    );
+  }
+}
+
+class ConGai extends StatelessWidget {
+
+  //count để hứng data từ ông bà
+  late int count;
+  //constructor
+  ConGai(this.count);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text("$count", style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
+
+
